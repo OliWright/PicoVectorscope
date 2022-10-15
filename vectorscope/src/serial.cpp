970fbfc7d632479a0ec9bb01db31aa96745fefa3
Oli Wright <oli.wright.github@gmail.com>
2022-10-15 15:52:52 +0100

    Ability to completely disable stdio

--- vectorscope/src/serial.cpp
+++ vectorscope/src/serial.cpp
@@ -3,8 +3,14 @@
 #include <stdio.h>
 #include "pico/bootrom.h"
 
-static repeating_timer_t s_timer;
+#if defined(_PICO_STDIO_USB_H) || LOG_ENABLED || defined(_PICO_STDIO_UART_H)
+#define ENABLE_SERIAL 1
+#else
+#define ENABLE_SERIAL 0
+#endif
 
+#if ENABLE_SERIAL
+static repeating_timer_t s_timer;
 static constexpr int32_t kFrequencyHz = 5;
 
 bool timerCallback(struct repeating_timer *t)
@@ -15,15 +21,16 @@ bool timerCallback(struct repeating_timer *t)
     }
     return true;
 }
+#endif
 
 void Serial::Init()
 {
-#if defined(_PICO_STDIO_USB_H) || LOG_ENABLED || defined(_PICO_STDIO_UART_H)
+#if ENABLE_SERIAL
     stdio_init_all();
     add_repeating_timer_us(-1000000 / kFrequencyHz, timerCallback, NULL, &s_timer);
-#endif
 #if defined(_PICO_STDIO_USB_H)
     sleep_ms(2000); //< Give a chance to attach a terminal
 #endif
     printf("\033[2J\033[H***********\nHello\n***********\n\n");
+#endif
 }
