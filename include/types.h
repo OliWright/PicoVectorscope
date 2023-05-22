24c2b3b7ee861c8ad686c580ebd274a4d96dd32b
Oli Wright <owright@nvidia.com>
2023-05-22 20:05:22 +0100

    Add some Vector3 operators

--- include/types.h
+++ include/types.h
@@ -43,6 +43,41 @@ struct Vector3
     Vector3() {}
     Vector3(ScalarType x, ScalarType y, ScalarType z) : x(x), y(y), z(z) {}
 
+    Vector3 operator + (const Vector3& rhs) const
+    {
+        return Vector3(x + rhs.x, y + rhs.y, z + rhs.z);
+    }
+
+    Vector3 operator - (const Vector3& rhs) const
+    {
+        return Vector3(x - rhs.x, y - rhs.y, z - rhs.z);
+    }
+
+    template<typename rhsScalarT>
+    Vector3 operator * (rhsScalarT rhs) const
+    {
+        return Vector3(x * rhs, y * rhs, z * rhs);
+    }
+
+    Vector3& operator += (const Vector3& rhs)
+    {
+        *this = *this + rhs;
+        return *this;
+    }
+
+    Vector3& operator -= (const Vector3& rhs)
+    {
+        *this = *this - rhs;
+        return *this;
+    }
+
+    template<typename rhsScalarT>
+    Vector3& operator *= (rhsScalarT rhs)
+    {
+        *this = *this * rhs;
+        return *this;
+    }
+
     const ScalarType& operator[](int idx) const { return (&x)[idx]; }
     ScalarType& operator[](int idx) { return (&x)[idx]; }
 };
