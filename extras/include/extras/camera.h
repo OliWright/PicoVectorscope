05a00a265b499e4774facf122ce1f7631c2e4f85
oli.wright.github@gmail.com <owright@nvidia.com>
2024-09-13 14:46:57 +0100

    Add `m_worldToCamera` matrix to `Camera` class

--- extras/include/extras/camera.h
+++ extras/include/extras/camera.h
@@ -43,7 +43,8 @@ public:
     // Calculate to update the internal fields.
     void Calculate()
     {
-        m_cameraToWorld.orthonormalInvert(m_worldToClip);
+        m_cameraToWorld.orthonormalInvert(m_worldToCamera);
+        m_worldToClip = m_worldToCamera;
         StandardFixedOrientationScalar scale = m_aspectRatio * m_recipTanHalfVerticalFOV;
         m_worldToClip.m[0][0] *= scale;
         m_worldToClip.m[1][0] *= scale;
@@ -58,6 +59,7 @@ public:
     const StandardFixedTranslationVector& GetPosition() const { return m_cameraToWorld.t; }
 
     const FixedTransform3D& GetCameraToWorld() const { return m_cameraToWorld; }
+    const FixedTransform3D& GetWorldToCamera() const { return m_worldToCamera; }
 
     // The worldToClip transform is used to transform points from world space to
     // clip space.
@@ -67,6 +69,7 @@ public:
 
 private:
     FixedTransform3D               m_cameraToWorld;
+    FixedTransform3D               m_worldToCamera;
     FixedTransform3D               m_worldToClip;
     StandardFixedOrientationScalar m_aspectRatio = 3.f / 4.f;
     StandardFixedOrientationScalar m_recipTanHalfVerticalFOV = 1.f;
